fd4ff7b32bcdc4e8091d6e422b2303dca5eb93d0
Thierno THIAM <thiernothiam@esp.sn>
2018-11-09 08:46:50 +0000

    Update module.py

--- module.py
+++ module.py
@@ -5,11 +5,11 @@ class parsing():
     """parsing the sentence from user to be able to query googlemap and wiki
     """
 
-    with open('static\localisation.txt', 'r') as file:
+    with open('.\static\localisation.txt', 'r') as file:
         texte = file.read()
         LOCALISATION = texte.split(',')
 
-    with open('static\stopwords.txt', 'r') as file:
+    with open('.\static\stopwords.txt', 'r') as file:
         texte = file.read()
         STOPWORDS = texte.split(',')
 
